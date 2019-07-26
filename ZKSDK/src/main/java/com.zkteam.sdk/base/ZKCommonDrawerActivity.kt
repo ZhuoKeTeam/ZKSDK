a73a321f4cf5fe912c941f01c9764f8d35ec1f2d
WangQing <wangqing11@100tal.com>
2019-07-26 12:04:03 +0800

    1. 添加 ZKTeam 的侧边栏。

--- ZKSDK/src/main/java/com.zkteam.sdk/base/ZKCommonDrawerActivity.kt
+++ ZKSDK/src/main/java/com.zkteam.sdk/base/ZKCommonDrawerActivity.kt
@@ -7,29 +7,37 @@ import android.widget.FrameLayout
 import androidx.annotation.LayoutRes
 import androidx.annotation.NonNull
 import androidx.drawerlayout.widget.DrawerLayout
-import com.blankj.utilcode.util.ToastUtils
 import com.google.android.material.navigation.NavigationView
 import com.zkteam.sdk.R
 
 abstract class ZKCommonDrawerActivity : ZKBaseActivity() {
 
-
-    protected lateinit var mBaseDrawerRootLayout: DrawerLayout
-    protected lateinit var mBaseDrawerContainerView: FrameLayout
+    lateinit var mBaseDrawerRootLayout: DrawerLayout
+    lateinit var mBaseDrawerContainerView: FrameLayout
 
     private val mListener = object : NavigationView.OnNavigationItemSelectedListener {
         override fun onNavigationItemSelected(@NonNull item: MenuItem): Boolean {
             val id = item.itemId
-            if (id == R.id.baseDrawerActionGitHub) {
-                ToastUtils.showShort("github")
-                return true
-            } else if (id == R.id.baseDrawerActionBlog) {
-                ToastUtils.showShort("blog")
-                return true
-            }
-            return false
+            return onDrawerItemClickListener(id)
         }
     }
+
+    /**
+     *  if (id == R.id.baseDrawerActionGitHub) {
+     *      ToastUtils.showShort("github")
+     *      return true
+     *  } else if (id == R.id.baseDrawerActionBlog) {
+     *      ToastUtils.showShort("blog")
+     *      return true
+     *  }
+     *  return false
+     */
+    abstract fun onDrawerItemClickListener(itemId: Int): Boolean
+
+    /**
+     * 布局请保留 R.layout.activity_zk_common_drawer 中的 id，否则可能无效，或者重新自行获取
+     */
+    abstract fun setDrawerLayout(): Int
 
     override fun isSwipeBack(): Boolean {
         return false
@@ -37,7 +45,12 @@ abstract class ZKCommonDrawerActivity : ZKBaseActivity() {
 
     @SuppressLint("ResourceType")
     override fun setRootLayout(@LayoutRes layoutId: Int) {
-        super.setRootLayout(R.layout.activity_zk_common_drawer)
+        val drawerLayoutId = setDrawerLayout()
+        if (drawerLayoutId == 0) {
+            super.setRootLayout(R.layout.activity_zk_common_drawer)
+        } else {
+            super.setRootLayout(drawerLayoutId)
+        }
         mBaseDrawerRootLayout = findViewById(R.id.baseDrawerRootLayout)
         mBaseDrawerContainerView = findViewById(R.id.baseDrawerContainerView)
         if (layoutId > 0) {
@@ -46,5 +59,7 @@ abstract class ZKCommonDrawerActivity : ZKBaseActivity() {
         val nav = findViewById<NavigationView>(R.id.baseDrawerNavView)
         nav.setNavigationItemSelectedListener(mListener)
     }
+
+
 
 }
