5393ddb4d00fc5489ce3208942148fed7d2244f2
WangQing <wangqing11@100tal.com>
2021-07-19 19:25:20 +0800

    1. 优化  IZKBaseView 类，添加说明，去除多余的注解； 2. 去除一些不需要的构建。

--- ZKSDK/src/main/java/com.zkteam.sdk/base/IZKBaseView.kt
+++ ZKSDK/src/main/java/com.zkteam.sdk/base/IZKBaseView.kt
@@ -2,14 +2,44 @@ package com.zkteam.sdk.base
 
 import android.os.Bundle
 import android.view.View
-import androidx.annotation.Nullable
 
-interface IZKBaseView {
+/**
+ * 【summary】     : 设置 View 的基础属性
+ * 【Create By】   : wangqing
+ * 【Create Time】 : 2021/7/19 6:49 下午
+ */
+internal interface IZKBaseView {
+    /**
+     * 设置资源 layout 的 id
+     * @return 资源 id
+     */
+    fun getLayoutId():Int
 
-    fun getLayoutId(): Int
-    fun initViews(@Nullable contentView: View)
+    /**
+     * 初始化 View
+     * @param contentView View
+     */
+    fun initViews(contentView: View?)
+
+    /**
+     * 设置 监听器
+     */
     fun initListener()
+
+    /**
+     * 设置 Lifecycle 的 Observe
+     */
     fun initLifecycleObserve()
-    fun initData(@Nullable bundle: Bundle?)
+
+    /**
+     * 初始化数据
+     * @param bundle Bundle
+     */
+    fun initData(bundle: Bundle?)
+
+    /**
+     * 防止 View 多次点击
+     * @param view View
+     */
     fun onDebouncingClick(view: View)
 }
